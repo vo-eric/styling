c15dab7dafa4064d25b9d468dece7a8b593ebfa4
Eric Vo <vo.eric@gmail.com>
2025-06-03 18:53:39 -0400

    Add rendering of boxes and adds a custom utility

--- src/components/GridBox.tsx
+++ src/components/GridBox.tsx
@@ -1,5 +1,38 @@
-function GridBox() {
-  return <div>GRID BOX</div>;
+import clsx from 'clsx';
+
+const determineColor = (weight: number): string => {
+  switch (weight) {
+    case 100:
+      return 'bg-grid-100';
+    case 200:
+      return 'bg-grid-200';
+    case 300:
+      return 'bg-grid-300';
+    case 400:
+      return 'bg-grid-400';
+    case 500:
+      return 'bg-grid-500';
+    case 600:
+      return 'bg-grid-100';
+    default:
+      return '';
+  }
+};
+
+function GridBox({ weight }: { weight: number }) {
+  return (
+    <div
+      className={clsx(
+        'h-[60px] w-[60px] justify-self-center',
+        determineColor(weight)
+      )}
+    />
+  );
 }
+
+/*
+
+bg-[#50d71e]
+*/
 
 export default GridBox;
--- src/components/HeatGrid.tsx
+++ src/components/HeatGrid.tsx
@@ -1,9 +1,16 @@
 import GridBox from './GridBox';
+import { createHeatGrid, DAYS } from '../data';
 
 function HeatGrid() {
   return (
-    <div>
-      <GridBox />
+    <div className='grid grid-cols-7-max gap-2 text-center font-medium'>
+      {DAYS.map((day) => {
+        return <p>{day}</p>;
+      })}
+
+      {createHeatGrid().map((weight) => (
+        <GridBox weight={weight} />
+      ))}
     </div>
   );
 }
