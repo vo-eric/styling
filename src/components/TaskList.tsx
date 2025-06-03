00119d9a8d8319d0f2f3ebdae14b8b959fcdf494
Eric Vo <vo.eric@gmail.com>
2025-06-03 13:12:39 -0400

    Move hard-coded data into its own file

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,39 +1,7 @@
 import Task from './Task';
 import { type Task as TaskType } from '../types';
 import { useState } from 'react';
-
-const TASKS: TaskType[] = [
-  {
-    title: 'Sweep the kitchen',
-    description: 'Get under the cabinets, do a good job',
-    completed: false,
-    id: 1,
-  },
-  {
-    title: 'Dishwashing',
-    description: 'Wash and dry dishes, pots, pans, and utensils',
-    completed: false,
-    id: 2,
-  },
-  {
-    title: 'Laundry',
-    description: 'Wash, dry, fold, and put away clothes and linens.',
-    completed: false,
-    id: 3,
-  },
-  {
-    title: 'Vacuuming',
-    description: 'Vacuum carpets, rugs, and floors throughout the house.',
-    completed: false,
-    id: 4,
-  },
-  {
-    title: 'Dusting',
-    description: 'Dust furniture, shelves, and other surfaces.',
-    completed: false,
-    id: 5,
-  },
-];
+import { TASKS } from '../data';
 
 const sortTasksByCompletion = (tasks: TaskType[]): TaskType[] => {
   const completedTasks = tasks.filter((task) => task.completed);
---
+++ src/data.ts
@@ -0,0 +1,34 @@
+import { type Task } from './types';
+
+export const TASKS: Task[] = [
+  {
+    title: 'Sweep the kitchen',
+    description: 'Get under the cabinets, do a good job',
+    completed: false,
+    id: 1,
+  },
+  {
+    title: 'Dishwashing',
+    description: 'Wash and dry dishes, pots, pans, and utensils',
+    completed: false,
+    id: 2,
+  },
+  {
+    title: 'Laundry',
+    description: 'Wash, dry, fold, and put away clothes and linens.',
+    completed: false,
+    id: 3,
+  },
+  {
+    title: 'Vacuuming',
+    description: 'Vacuum carpets, rugs, and floors throughout the house.',
+    completed: false,
+    id: 4,
+  },
+  {
+    title: 'Dusting',
+    description: 'Dust furniture, shelves, and other surfaces.',
+    completed: false,
+    id: 5,
+  },
+];
